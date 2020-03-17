0c1a7f0ea62e072695fff47fd4a76bb02f4211b1
sravanthi33 <62071167+sravanthi33@users.noreply.github.com>
2020-03-17 10:55:02 +0530

    Add files via upload

---
+++ SwitchCase.java
@@ -0,0 +1,21 @@
+class SwitchCase
+{
+public static void main(String a[])
+{
+int i=4;
+switch(i)
+{
+   case 0:
+       System.out.println("zero");
+       break;
+   case 1:
+       System.out.println("one");
+       break;
+   case 4:
+       System.out.println("four");
+       break;
+   default:
+       System.out.println("invalid number");
+}
+}
+}
