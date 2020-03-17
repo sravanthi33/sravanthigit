18821cc97b093e03df7cf9fea80424763dab2177
sravanthi33 <62071167+sravanthi33@users.noreply.github.com>
2020-03-17 19:36:21 +0530

    Add files via upload

---
+++ PatternFour.java
@@ -0,0 +1,15 @@
+class PatternFour
+{
+public static void main(String a[])
+{
+  for(int i=1;i<=4;i++)
+  {
+    for(int j=1;j<=4;j++)
+    {
+       System.out.print("*");
+     }
+       System.out.println(" ");
+    }
+   System.out.println(" ");
+}  
+}
