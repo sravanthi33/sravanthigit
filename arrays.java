e34b6da767b6cddf236fbaee549779e290773fc6
sravanthi33 <62071167+sravanthi33@users.noreply.github.com>
2020-03-18 08:04:20 +0530

    Add files via upload

---
+++ arrays.java
@@ -0,0 +1,12 @@
+class arrays
+{
+public static void main(String args[])
+{
+ int a[ ] =new int[3] ;
+    a[0]=1;
+    a[1]=2;
+    a[2]=3;
+    for(int i=0;i<3;i++)
+    System.out.println(a[i]);
+}
+}
