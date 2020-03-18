0af62e6d3eb25c7f9fab1a9e50575004ba78e28a
sravanthi33 <62071167+sravanthi33@users.noreply.github.com>
2020-03-18 18:42:44 +0530

    Add files via upload

---
+++ constructor.java
@@ -0,0 +1,14 @@
+class constructor
+{
+static public void main(String[] args)
+{ 
+    ghi obj = new ghi();
+   }
+}
+  class ghi
+  {
+  public ghi()
+    {
+       System.out.println("ghi Const");
+    }
+  }
