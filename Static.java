add040bf66260778fade484706a6d0fb07afffa2
sravanthi33 <62071167+sravanthi33@users.noreply.github.com>
2020-03-20 07:39:03 +0530

    Add files via upload

---
+++ Static.java
@@ -0,0 +1,17 @@
+public class Static
+{
+  static public void main (String[] args)
+  {
+     lmn.show();
+    
+   }
+ }
+ class lmn
+ {
+       static int k= 9;
+       public static void show()
+       {
+             System.out.println("hello");
+             System.out.println(k);
+       }
+  }
