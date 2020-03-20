a7a08d20d5f8e693f1e6319b9abb9e76abd53e60
sravanthi33 <62071167+sravanthi33@users.noreply.github.com>
2020-03-20 10:22:43 +0530

    Add files via upload

---
+++ Staticthree.java
@@ -0,0 +1,19 @@
+public class Staticthree
+{
+ static
+ {
+   
+    System.out.print("hii");
+  }
+  public static void main (String[] args)
+  {
+ 
+    System.out.print("how are");
+    
+   }
+ static
+ {
+   
+    System.out.print("you");
+}
+}
